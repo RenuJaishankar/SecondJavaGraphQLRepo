b0b8254a849073cd61101c53121aaf690da5fa8f
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-01 13:57:43 -0400

    CORS fixed

--- src/main/java/com/example/demo/CORSFilter.java
+++ src/main/java/com/example/demo/CORSFilter.java
@@ -13,9 +13,9 @@ public class CORSFilter implements Filter {
 public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException{
            HttpServletResponse response = (HttpServletResponse) servletResponse;
            response.setHeader("Access-Control-Allow-Origin","*");
-            response.setHeader("Access-Control-Allow_Methods","POST, GET, PUT, OPTIONS, DELETE, PATCH");
+            response.setHeader("Access-Control-Allow-Methods","POST, GET, PUT, OPTIONS, DELETE, PATCH");
             response.setHeader("Access-Control-Max-Age","3600");
-                            response.setHeader("Access-Control-Allow_Headers","Origin,Content-Type,Accept");
+                            response.setHeader("Access-Control-Allow-Headers","Origin,Content-Type,Accept");
             response.setHeader("Access-Control-Allow-Credentials","true");
             filterChain.doFilter(servletRequest,servletResponse);
     }
