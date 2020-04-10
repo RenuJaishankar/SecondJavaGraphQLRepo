ca304187a73de344467dfaead9ca66c6f8e74ead
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-10 11:36:12 -0400

    fruitposts table linked to model

--- src/main/java/com/example/demo/Model/FruitPost.java
+++ src/main/java/com/example/demo/Model/FruitPost.java
@@ -1,12 +1,10 @@
 package com.example.demo.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table(name="fruitposts")
 public class FruitPost {
 @Id
 @GeneratedValue (strategy = GenerationType.IDENTITY)
