fdaa20bc4580e712266efc2f4f94f02948e115ce
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-12 17:37:46 -0400

    second commit

--- src/main/java/com/example/demo/Repo/PlacepostRepo.java
+++ src/main/java/com/example/demo/Repo/PlacepostRepo.java
@@ -3,6 +3,6 @@ package com.example.demo.Repo;
 import com.example.demo.Model.PlacePost;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlacepostRepo extends crudRepository<PlacePost,Long){
+public interface PlacepostRepo extends CrudRepository<PlacePost,Long>{
 }
 
