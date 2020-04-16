c26d3e057013b92557a576f9668584ea680e714c
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-16 15:39:07 -0400

    Post repo changed

--- src/main/java/com/example/demo/Repo/PostRepo.java
+++ src/main/java/com/example/demo/Repo/PostRepo.java
@@ -1,9 +1,9 @@
 package com.example.demo.Repo;
 
 import com.example.demo.Model.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.Repository;
-
+//import org.springframework.data.repository.Repository;
+import java.util.List;
 public interface PostRepo extends PagingAndSortingRepository<Post,Long> {
 }
