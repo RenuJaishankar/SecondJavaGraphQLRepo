6a49d0ed1439629c97cd0878102474eed57b40d4
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-04-19 18:38:02 -0400

    getPages added to placepost

--- src/main/java/com/example/demo/Repo/PlacepostRepo.java
+++ src/main/java/com/example/demo/Repo/PlacepostRepo.java
@@ -1,8 +1,11 @@
 package com.example.demo.Repo;
 
 import com.example.demo.Model.PlacePost;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PlacepostRepo extends CrudRepository<PlacePost,Long>{
+//public interface PlacepostRepo extends CrudRepository<PlacePost,Long>{
+public interface PlacepostRepo extends PagingAndSortingRepository<PlacePost,Long> {
 }
 
